97911c4a0b5868d63a0e24972ecff768a8731d4c
Maddie Pfau <madbird67.joy@gmail.com>
2017-06-20 19:20:55 +0000

    Methods practice complete

---
+++ ruby2.rb
@@ -0,0 +1,19 @@
+def timesByOneThousand(number)
+    number*1000
+end
+puts timesByOneThousand(20)
+
+def howOldIwillBein2030(number)
+    2030 - (2017 -number)
+end
+puts howOldIwillBein2030(17)
+
+def makeExcited(string)
+    string.upcase + "!!!"
+end
+puts makeExcited("hello")
+
+def happyBackwards(string)
+    string.reverse + ":)"
+end
+puts happyBackwards("cool")
